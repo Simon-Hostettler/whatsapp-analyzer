85c0a188f39d48166cea8a03e0e0196c38638399
Simon Hostettler <Simon Hostettler>
2023-04-22 15:39:09 +0200

    changed formatter to black

--- text_analysis.py
+++ text_analysis.py
@@ -4,8 +4,11 @@ import emoji
 
 
 def most_common_emojis(messages, n=5):
-    def emojis_per_mess(message): return "".join(
-        list(map(lambda dict: dict["emoji"][0], emoji.emoji_list(message))))
+    def emojis_per_mess(message):
+        return "".join(
+            list(map(lambda dict: dict["emoji"][0], emoji.emoji_list(message)))
+        )
+
     emojis = "".join(list(map(emojis_per_mess, messages)))
     return collections.Counter(emojis).most_common(n)
 
@@ -15,7 +18,9 @@ def num_messages(messages):
 
 
 def avg_message_wc(messages):
-    return sum(map(lambda mess: len(text_only(mess).split(" ")) - 1, messages)) / len(messages)
+    return sum(map(lambda mess: len(text_only(mess).split(" ")) - 1, messages)) / len(
+        messages
+    )
 
 
 def text_only(message):
@@ -25,7 +30,14 @@ def text_only(message):
 def get_datetime(message):
     date = list(map(int, (message.split(",")[0][1:]).split(".")))
     time = list(map(int, (message.split("]")[0].split(",")[1]).split(":")))
-    return datetime.datetime(day=date[0], month=date[1], year=date[2] + 2000, hour=time[0], minute=time[1], second=time[2])
+    return datetime.datetime(
+        day=date[0],
+        month=date[1],
+        year=date[2] + 2000,
+        hour=time[0],
+        minute=time[1],
+        second=time[2],
+    )
 
 
 def get_date(message):
