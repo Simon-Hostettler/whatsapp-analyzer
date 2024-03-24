4a281c2d31fb6c0d0cb37bff609277f0927abe0a
Simon Hostettler <simon.hostettler@protonmail.com>
2024-03-24 12:01:31 +0100

    only switch backend on macOS

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import seaborn as sns
 from loading import *
 from poster import *
 from plt_stylesheet import stylesheet
+from platform import system
 
 
 if __name__ == "__main__":
     # change backend to render emojis correctly
-    matplotlib.use("module://mplcairo.macosx")
+    if system().lower() == "darwin":
+        matplotlib.use("module://mplcairo.macosx")
 
     plt.rcParams.update(stylesheet)
 
     create_folders_if_missing()
 
     # load chat
-    chat = load_file("chat.txt")
+    chat = load_file("_chat 11.txt")
     user1, user2, u1_messages, u2_messages = split_user_messages(chat)
 
     create_poster(user1, user2, u1_messages, u2_messages)
