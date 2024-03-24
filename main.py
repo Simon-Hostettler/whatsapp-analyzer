df23177b412ab9b3ec46b2dc47f90c07140e2f36
Simon Hostettler <simon.hostettler@protonmail.com>
2024-03-24 12:02:34 +0100

    only switch backend on macOS

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from platform import system
 
 
 if __name__ == "__main__":
+
     # change backend to render emojis correctly
     if system().lower() == "darwin":
         matplotlib.use("module://mplcairo.macosx")
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     create_folders_if_missing()
 
     # load chat
-    chat = load_file("_chat 11.txt")
+    chat = load_file("chat.txt")
     user1, user2, u1_messages, u2_messages = split_user_messages(chat)
 
     create_poster(user1, user2, u1_messages, u2_messages)
